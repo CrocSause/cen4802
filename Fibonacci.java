7f199b78d2c592ebc7e07d2c502e853d59e96030
Mitchell <78571607+CrocSause@users.noreply.github.com>
2024-08-28 19:15:38 -0400

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -13,7 +13,13 @@ public class Fibonacci {
 		input.close();
 
 	}
-	
+	/**
+	 * Returns the nth term of the Fibonacci sequence.
+	 * @param n this term in the Fibonacci sequence to be calculated.
+	 * @return 0 the 0th term is 0.
+	 * @return 1 the 1st and 2nd term are 1.
+	 * @return 	 recursive call to fibonacciSequence method for terms > 2.
+	 */
 	public static int fibonacciSequence(int n) {
 		
 		if (n==0) 
